1c6eefbff12a4a1f7bfc5f4ae7d9dd8a7388c815
Lyxsly <lyxs7y@gmail.com>
2026-03-29 15:39:19 +0900

    16日目：今更2026年の目標を立ててみた。

---
+++ ABC046/b.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+// #include <vector>
+// #include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int N, K;
+    cin >> N >> K;
+    int ans = K;
+    for (int i = 1; i < N; i++)
+    {
+        ans *= K - 1;
+    }
+    cout << ans << endl;
+    return 0;
+}
